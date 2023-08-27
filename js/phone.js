fa88c5ceefafffd27d17029d833173925649cac1
sazzadul1205 <psazzadul@gmail.com>
2023-08-27 20:27:09 +0600

    34-2

--- js/phone.js
+++ js/phone.js
@@ -3,6 +3,37 @@ const loadPhone = async () => {
     const data = await res.json();
     const phones = data.data
     console.log(phones);
+    displayPhones(phones);
 }
+
+const displayPhones = phones => {
+    // console.log('phones');
+    // get the location
+    const phoneContainer = document.getElementById('phone-container')
+    phones.forEach(phone => {
+        console.log(phone);
+        // 2. create a div
+        const phoneCard = document.createElement('div');
+        phoneCard.classList = `card w-96 bg-gray-100 shadow-xl`;
+        // 3. set inner HTML
+        phoneCard.innerHTML = `
+        <figure class="px-10 pt-10">
+            <img src="${phone.image}" alt="Shoes" class="rounded-xl" />
+        </figure>
+        <div class="card-body items-center text-center">
+            <h2 class="card-title">${phone.phone_name}</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div class="card-actions">
+                <button class="btn btn-primary">Buy Now <button>
+            </div>
+        </div>
+        `
+        // 4. append Child
+        phoneContainer.appendChild(phoneCard)
+    });
+}
+
+
+
 
 loadPhone()
